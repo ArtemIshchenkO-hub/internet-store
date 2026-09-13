52edd342c2efe025a9714f387c2f481b7f67d94d
ArtemIshchenkO-hub <nordeptanah8@gmail.com>
2026-09-13 14:03:29 +0000

    Deploying to gh-pages from @ ArtemIshchenkO-hub/internet-store@282a7633d21f3bcef01079e7049437424a808216 🚀

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-import"./assets/styles-JE8YjOlG.js";import{a as s}from"./assets/vendor-N5iQpiFS.js";function y(){document.querySelector(".categories__btn").classList.add("categories__btn--active")}function _(t){document.querySelector(".categories__btn--active").classList.remove("categories__btn--active"),t.classList.add("categories__btn--active")}const m="https://dummyjson.com",r={categories:"/products/category-list",products:"/products",productsByCategory:"/products/category"},a=12;s.defaults.baseURL=m;async function f(){const{data:t}=await s.get(r.categories);return t}async function n(t){const{data:e}=await s.get(r.products,{params:{limit:a,skip:(t-1)*a}});return e}async function b(t){const{data:e}=await s.get(`${r.productsByCategory}/${t}`);return e}const c={categoryList:document.querySelector(".categories"),productsList:document.querySelector(".products"),notFound:document.querySelector(".not-found")};function L(t){const e=t.map(o=>`
+import"./assets/styles-JE8YjOlG.js";import{a as s,i as f}from"./assets/vendor-ZnMw6IGI.js";function h(){document.querySelector(".categories__btn").classList.add("categories__btn--active")}function b(t){document.querySelector(".categories__btn--active").classList.remove("categories__btn--active"),t.classList.add("categories__btn--active")}const L="https://dummyjson.com",a={categories:"/products/category-list",products:"/products",productsByCategory:"/products/category",searchProduct:"/products/search"},m=12;s.defaults.baseURL=L;async function P(){const{data:t}=await s.get(a.categories);return t}async function g(t){const{data:o}=await s.get(a.products,{params:{limit:m,skip:(t-1)*m}});return o}async function $(t){const{data:o}=await s.get(`${a.productsByCategory}/${t}`);return o}async function B(t){const{data:o}=await s.get(`${a.products}/${t}`);return o}async function v(t){const{data:o}=await s.get(a.searchProduct,{params:{q:t}});return o}const e={categoryList:document.querySelector(".categories"),productsList:document.querySelector(".products"),notFound:document.querySelector(".not-found"),searchForm:document.querySelector(".search-form"),clearBtn:document.querySelector(".search-form__btn-clear"),modal:document.querySelector(".modal"),modalProduct:document.querySelector(".modal-product")};function w(t){const o=t.map(c=>`
         <li class="categories__item">
-            <button class="categories__btn" type="button">${o}</button>
+            <button class="categories__btn" type="button">${c}</button>
         </li>
-        `).join("");c.categoryList.insertAdjacentHTML("beforeend",e)}function i(t){const e=t.map(({title:o,brand:u,category:d,price:l,thumbnail:g,id:p})=>` <li class="products__item" data-id="${p}">
-    <img class="products__image" src="${g}" alt="${o}"/>
-    <p class="products__title">${o}</p>
-    <p class="products__brand"><span class="products__brand--bold">Brand: ${u}</span></p>
-    <p class="products__category">Category: ${d}</p>
-    <p class="products__price">Price: ${l}$</p>
+        `).join("");e.categoryList.insertAdjacentHTML("beforeend",o)}function l(t){const o=t.map(({title:c,brand:r,category:n,price:i,thumbnail:d,id:u})=>` <li class="products__item" data-id="${u}">
+    <img class="products__image" src="${d}" alt="${c}"/>
+    <p class="products__title">${c}</p>
+    <p class="products__brand"><span class="products__brand--bold">Brand: ${r}</span></p>
+    <p class="products__category">Category: ${n}</p>
+    <p class="products__price">Price: ${i}$</p>
  </li>
 
-    `).join("");c.productsList.insertAdjacentHTML("beforeend",e)}function C(){c.productsList.innerHTML=""}async function $(){try{const t=await f();L(["All",...t]),y()}catch(t){console.log(t.message)}}async function B(){try{const{products:t}=await n(1);i(t)}catch(t){console.log(t.message)}}async function P(t){if(t.target.nodeName!=="BUTTON")return;const e=t.target.textContent;_(t.target);try{let o=null;if(C(),e==="All"?o=await n(1):o=await b(e),o.products.length===0){c.notFound.classList.add("not-found--visible");return}i(o.products)}catch(o){console.log(o.message)}}$();B();c.categoryList.addEventListener("click",P);
+    `).join("");e.productsList.insertAdjacentHTML("beforeend",o)}function p(){e.productsList.innerHTML=""}function C(t){const{title:o,tags:c,description:r,shippingInformation:n,returnPolicy:i,price:d,thumbnail:u}=t,_=`
+    <img class="modal-product__img" src="${u}" alt="${o}" />
+      <div class="modal-product__content">
+        <p class="modal-product__title">${o}</p>
+        <ul class="modal-product__tags">${c}</ul>
+        <p class="modal-product__description">${r}</p>
+        <p class="modal-product__shipping-information">Shipping:${n}</p>
+        <p class="modal-product__return-policy">Return Policy:${i}</p>
+        <p class="modal-product__price">Price: ${d}$</p>
+        <button class="modal-product__buy-btn" type="button">Buy</button>
+      </div>`;e.modalProduct.innerHTML=_}async function S(){try{const t=await P();w(["All",...t]),h()}catch(t){console.log(t.message)}}async function y(){try{const{products:t}=await g(1);l(t)}catch(t){console.log(t.message)}}async function q(t){if(t.target.nodeName!=="BUTTON")return;const o=t.target.textContent;b(t.target);try{let c=null;if(p(),o==="All"?c=await g(1):c=await $(o),c.products.length===0){e.notFound.classList.add("not-found--visible");return}l(c.products)}catch(c){console.log(c.message)}}async function T(t){t.preventDefault();const c=t.currentTarget.elements.searchValue.value.trim();if(!c){f.warning({message:"Enter something to search"});return}try{p();const{products:r}=await v(c);r.length?e.notFound.classList.remove("not-found--visible"):e.notFound.classList.add("not-found--visible"),l(r)}catch(r){console.log(r.message)}}async function E(){e.searchForm.reset(),p(),await y()}async function k({target:t}){if(!t.closest("li"))return;const o=t.dataset.id;if(o)try{const c=await B(o);C(c),e.modal.classList.add("modal--is-open")}catch(c){console.log(c.message)}}S();y();e.categoryList.addEventListener("click",q);e.searchForm.addEventListener("submit",T);e.clearBtn.addEventListener("click",E);e.productsList.addEventListener("click",k);
 //# sourceMappingURL=index.js.map
